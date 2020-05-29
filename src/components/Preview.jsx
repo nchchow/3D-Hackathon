8ddf98e56f5b3dd988b6fc5fe077a81f80076d7e
Monica Zhazil <monicazhazil@MacBook-Pro.hitronhub.home>
2020-05-29 10:40:53 -0700

    Created Components Folder and Preview Component.

---
+++ src/components/Preview.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Preview (){
+    return(
+        <span>This could be a preview for a Business to showcase what they have available.</span>
+        <h1>Business Name</h1>
+        <p>About</p>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    )
+}
+
+export default Preview;
